13089aee527811a880276c4dc896728e36b94256
Ackincolor <lois.guillet@gmail.com>
2018-10-24 17:31:08 +0200

    ajout asciiART

--- src/main/java/com/Ackincolor/App.java
+++ src/main/java/com/Ackincolor/App.java
@@ -11,6 +11,39 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("                  /88888888888888888888888888\\\n" +
+                "                  |88888888888888888888888888/\n" +
+                "                   |~~____~~~~~~~~~\"\"\"\"\"\"\"\"\"|\n" +
+                "                  / \\_________/\"\"\"\"\"\"\"\"\"\"\"\"\"\\\n" +
+                "                 /  |              \\         \\\n" +
+                "                /   |  88    88     \\         \\\n" +
+                "               /    |  88    88      \\         \\\n" +
+                "              /    /                  \\        |\n" +
+                "             /     |   ________        \\       |\n" +
+                "             \\     |   \\______/        /       |\n" +
+                "  /\"\\         \\     \\____________     /        |\n" +
+                "  | |__________\\_        |  |        /        /\n" +
+                "/\"\"\"\"\\           \\_------'  '-------/       --\n" +
+                "\\____/,___________\\                 -------/\n" +
+                "------*            |                    \\\n" +
+                "  ||               |                     \\\n" +
+                "  ||               |                 ^    \\\n" +
+                "  ||               |                | \\    \\\n" +
+                "  ||               |                |  \\    \\\n" +
+                "  ||               |                |   \\    \\\n" +
+                "  \\|              /                /\"\"\"\\/    /\n" +
+                "     -------------                |    |    /\n" +
+                "     |\\--_                        \\____/___/\n" +
+                "     |   |\\-_                       |\n" +
+                "     |   |   \\_                     |\n" +
+                "     |   |     \\                    |\n" +
+                "     |   |      \\_                  |\n" +
+                "     |   |        ----___           |\n" +
+                "     |   |               \\----------|\n" +
+                "     /   |                     |     ----------\"\"\\\n" +
+                "/\"\\--\"--_|                     |               |  \\\n" +
+                "|_______/                      \\______________/    )\n" +
+                "                                              \\___/");
         System.out.println("Quel est votre nom ?");
         String name = scanner.next();
         System.out.println("OK :)");
@@ -21,5 +54,6 @@ public class App
         Utilisateur joueur = new Utilisateur(name,diffpls,diffmoin);
         GameEngine gm = new GameEngine(scanner,new ServiceRandom());
         while(gm.play(joueur));
+        System.out.println("GOODBY!!!!");
     }
 }
